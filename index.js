d18eed4bbcfabfcffba9e444837b714c788c63ac
Klitos Giannakopoulos <klitos@duckseason.mobi>
2021-12-30 23:05:41 +0200

    Add implementation for command request

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { readFileSync } from 'fs'
+import { format } from 'util'
+import { exec } from 'child_process'
 
 const port = 9977;
 
@@ -11,6 +13,11 @@ const PARAM_FLOAT = "float"
 const PARAM_TEXT = "text"
 const PARAM_BOOLEAN = "boolean"
 const ACCEPTED_PARAMETER_TYPES = [PARAM_INTEGER, PARAM_FLOAT, PARAM_BOOLEAN, PARAM_TEXT]
+const ACCEPTED_BOOLEAN_TRUE_VALUES = ["true", "t", "1"]
+const ACCEPTED_BOOLEAN_FALSE_VALUES = ["false", "f", "0"]
+const COMMAND_RESPONSE_BAD_REQUEST_BODY = "<html><body><h1>[400] Bad Request</h1>%s</body></html>\n"
+const COMMAND_RESPONSE_OK_BODY = "<html><body><h1>[200] OK</h1><p>Command successful</p></body></html>\n"
+
 
 let supported_commands = {}
 
@@ -60,6 +67,103 @@ app.get('/commands', (req, res) =>  {
         delete parameters.shellCommand
     }
     res.send(JSON.stringify(clone, null, 2) + "\n")
+})
+
+app.get('/command/:commandName', (req, res) => {   
+    const command_name = req.params.commandName;
+
+    console.log("Command <" + command_name + "> requested. Trying to parse parameters.")
+
+    // let command =supported_commands[command_name] //use case insensitive key finding below instead
+    const command = supported_commands[Object.keys(supported_commands).find(key => key.toLowerCase() === command_name.toLowerCase())];
+    if (typeof command === 'undefined') {
+        const message = format(COMMAND_RESPONSE_BAD_REQUEST_BODY, "<p>Command not found<p>")
+        res.status(400).send(message)
+        return
+    }
+    let shellCommand = command.shellCommand
+    console.log("-> " + shellCommand)
+
+    for (const [param, expectedType] of Object.entries(command)) {
+        if (param === PARAM_SHELL_COMMAND) {
+            continue
+        }
+
+        if (req.query.hasOwnProperty(param)){
+            const value = req.query[param]
+            
+            //validate value
+            switch(expectedType) {
+                case PARAM_TEXT:
+                    break
+                case PARAM_INTEGER:
+                    if (!Number.isSafeInteger(Number.parseInt(value))){
+                        const reason = "Unable to convert value of param " + param + " to type " + expectedType
+                        sendErrorResponse(reason)
+                        return
+                    }
+                    break
+                case PARAM_FLOAT:
+                    const number = Number.parseFloat(value);
+                    if (Number(number) !== number){
+                        const reason = "Unable to convert value of param " + param + " to type " + expectedType
+                        sendErrorResponse(reason)
+                        return
+                    }
+                    break
+                case PARAM_BOOLEAN:
+                    if (getBooleanValue(value) == undefined) {
+                        const reason = "Unable to convert value of param " + param + " to type " + expectedType
+                        sendErrorResponse(reason)
+                        return
+                    }
+                    break
+            }
+
+            shellCommand = shellCommand.replace("$" + param, value)
+
+        } else {
+            const reason = "missing param " + param
+            sendErrorResponse(reason)
+            return
+        }
+    }
+
+    console.log("Parameters parsed successfully, responding OK\n")
+
+    executeShellCommand(shellCommand);
+    res.send(COMMAND_RESPONSE_OK_BODY)
+
+
+    function sendErrorResponse(reason) {
+        console.log(reason + ". Responding with an error.");
+        const message = format(COMMAND_RESPONSE_BAD_REQUEST_BODY, `<p>${reason}<p>`);
+        res.status(400).send(message);
+    }
+
+    function executeShellCommand(cmd) {
+        exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
+    }
+
+    function getBooleanValue(x) {
+        if (ACCEPTED_BOOLEAN_TRUE_VALUES.includes(x.toLowerCase())) {
+            return true
+        } else if (ACCEPTED_BOOLEAN_FALSE_VALUES.includes(x.toLowerCase())) {
+            return false
+        } else {
+            return undefined
+        }
+    }
 })
 
 app.listen(port, () => {
